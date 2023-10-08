dd4847cde3b3e260bb99fece45d8c671ed03d9b3
alextamboli <alex.tamboli8@gmail.com>
2023-10-09 00:50:34 +0530

    admin site setup - semi complete

--- playground/views.py
+++ playground/views.py
@@ -12,10 +12,10 @@ from tags.models import TaggedItem
 def say_hello(request):
     # queryset = Product.objects.raw("SELECT id, title FROM store_product ORDER BY title")
 
-    collection = Collection()
-    collection.title = 'test 2'
-    collection.featured_product = Product(pk=2)
-    collection.save()
+    # collection = Collection()
+    # collection.title = 'test 2'
+    # collection.featured_product = Product(pk=2)
+    # collection.save()
     
     # collection = Collection.objects.create(title='Video Games', featured_product=Product.objects.get(pk=1))
     context = {
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,96 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.db.models import Count
+from django.http.request import HttpRequest
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+from . import models
 
-# Register your models here.
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'Inventory'
+    parameter_name = 'inventory'
+    
+    def lookups(self, request: Any, model_admin: Any) -> list[tuple[Any, str]]:
+        return [
+            ('<10', 'Low'),
+            ('>10', 'OK')
+            ]
+        
+    def queryset(self, request: Any, queryset: QuerySet[Any]) -> QuerySet[Any] | None:
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+        elif self.value() == '>10':
+            return queryset.filter(inventory__gte=10)
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_select_related = ['collection']
+    list_filter = ['collection', 'last_update', InventoryFilter]
+    list_per_page = 10
+    
+    def collection_title(self, product):
+        return product.collection.title
+     
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        else: return 'OK'
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
+    list_editable = ['membership']
+    list_per_page = 20
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+    
+    @admin.display(ordering='ordercount')
+    def orders_count(self, customer):
+        url = (reverse('admin:store_order_changelist')
+                + '?'
+                + urlencode({
+                    'customer__id': str(customer.id),
+                })
+               )
+        return format_html('<a href={}>{}</a>',
+                           url,
+                           customer.ordercount)
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(
+            ordercount = Count('order')
+        )
+
+    
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+    
+    
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(
+            products_count = Count('product')
+        )
+    
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = (reverse('admin:store_product_changelist')
+               + '?'
+               + urlencode({
+                   'collection__id': str(collection.id),
+               })
+            )
+        return format_html('<a href="{}"> {} </a>',
+                           url,
+                           collection.products_count)
+
+    
