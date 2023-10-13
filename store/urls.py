ca4cb338a4ad48b00f628f3e253581e4e1b8989c
alextamboli <alex.tamboli8@gmail.com>
2023-10-13 11:28:59 +0530

    Cart API added

--- store/urls.py
+++ store/urls.py
@@ -5,13 +5,17 @@ from . import views
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
+router.register('carts',views.CartViewSet, basename='carts')
 
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
 
+carts_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+carts_router.register('items', views.CartItemViewSet, basename='cart-items')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(products_router.urls)),
+    path('', include(carts_router.urls)),
     # path('products/', views.ProductList.as_view()), and more as per ur need
 ]
